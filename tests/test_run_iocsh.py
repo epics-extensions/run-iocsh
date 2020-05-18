e1dca21096a3ef6a00fd30aa945570e1291b3dd9
Simon Rose <simon.rose@ess.eu>
2020-05-18 15:26:40 +0200

    Added new tests

--- tests/test_run_iocsh.py
+++ tests/test_run_iocsh.py
@@ -2,7 +2,15 @@ import logging
 import pytest
 import re
 from click.testing import CliRunner
-from run_iocsh import run_iocsh, main, IocshModuleNotFoundError, IocshTimeoutExpired
+from run_iocsh import (
+    run_iocsh,
+    main,
+    IocshModuleNotFoundError,
+    IocshTimeoutExpired,
+    IOC,
+)
+from epics import PV
+from time import sleep
 
 
 def get_module_dir(logtext: str, module: str):
@@ -80,6 +88,31 @@ def test_run_iocsh_acf_file_not_found(caplog):
     assert "No such file or directory: '{}/unknown_access.acf'".format(ess_dir) == str(
         excinfo.value
     )
+
+
+def test_split_run():
+    ioc = IOC()
+    assert not ioc.is_running()
+
+    ioc.run("iocsh.bash")
+    assert ioc.is_running()
+
+    ioc.exit()
+    assert not ioc.is_running()
+
+
+def test_runiocsh_with_pvaccess():
+    ioc = IOC()
+    ioc.run("iocsh.bash", "tests/cmds/test_pv.cmd")
+
+    pv = PV("TEST")
+    assert int(pv.get()) == 5
+
+    pv.put("17")
+    sleep(1)
+    assert int(pv.get()) == 17
+
+    ioc.exit()
 
 
 @pytest.mark.parametrize("name", ("iocsh-timeout.bash", "iocsh-stdin-closed.bash"))
