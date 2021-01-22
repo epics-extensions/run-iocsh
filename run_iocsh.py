4322e30e2382247809942d2cf243788d7c5efea6
anders lindh olsson <anders.lindholsson@ess.eu>
2021-01-22 16:46:29 +0100

    update ci/cd job names, add isort configuration, and apply pre-commit

--- run_iocsh.py
+++ run_iocsh.py
@@ -18,14 +18,15 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import click
+
 import logging
 import re
+import shutil
 import subprocess
 import sys
 import time
-import shutil
 
+import click
 
 RE_MODULE_NOT_AVAILABLE = re.compile("Module .*? not available")
 RE_CANT_OPEN = re.compile(r"(save_restore:)?\s*Can't\s*open\s*(.*?):")
