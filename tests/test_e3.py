934f4fb242162564279a28d89d163aa6c8860b38
Anders Lindh Olsson <anders.lindholsson@ess.eu>
2024-11-14 09:46:33 +0100

    E3-1719: Break bidirectional dependencies
    Remove dependencies on essioc, iocstats, and autosave, which all depend
    on run-iocsh implicitly via require's test facilities.
    
    Patches integration tests with mock objects for subprocess comms.
    
    Replaces test for missing file exception.

--- tests/test_e3.py
+++ tests/test_e3.py
@@ -1,9 +1,20 @@
 import logging
 from pathlib import Path
+from typing import Tuple
+from unittest.mock import patch
 
 import pytest
 
 from run_iocsh import run_iocsh
+
+
+def mocked_iocsh_subprocess_communicate_retval(module_name: str) -> Tuple[bytes, bytes]:
+    outs = f"""\
+Loaded {module_name} version not-a-real-version
+Loading module info records for {module_name}
+""".encode()
+    errs = b""
+    return outs, errs
 
 
 def test_run_iocsh_no_args(caplog: pytest.LogCaptureFixture) -> None:
@@ -14,38 +25,37 @@ def test_run_iocsh_no_args(caplog: pytest.LogCaptureFixture) -> None:
 
 
 def test_run_iocsh_load_module(caplog: pytest.LogCaptureFixture) -> None:
-    with caplog.at_level(logging.INFO):
-        run_iocsh("iocsh", 2, "-r", "iocstats")
-    assert "Loaded iocstats version" in caplog.text
-    assert "Loading module info records for iocstats" in caplog.text
+    module_name = "mock"
+    with caplog.at_level(logging.INFO), patch("subprocess.Popen") as popen_mock:
+        process_mock = popen_mock.Mock()
+        process_mock.returncode = 0
+        process_mock.communicate.return_value = (
+            mocked_iocsh_subprocess_communicate_retval(module_name)
+        )
+        popen_mock.return_value = process_mock
+
+        run_iocsh("iocsh", 2, "-r", module_name)
+    assert f"Loaded {module_name} version" in caplog.text
+    assert f"Loading module info records for {module_name}" in caplog.text
 
 
 def test_run_iocsh_module_loading(
     tmp_path: Path, caplog: pytest.LogCaptureFixture
 ) -> None:
-    file_contents = """\
-require iocstats
+    module_name = "mock"
+    file_contents = f"""\
+require {module_name}
 """
     tmp_file = tmp_path / "test_module_loading.cmd"
     tmp_file.write_text(file_contents)
 
-    with caplog.at_level(logging.INFO):
+    with caplog.at_level(logging.INFO), patch("subprocess.Popen") as popen_mock:
+        process_mock = popen_mock.Mock()
+        process_mock.returncode = 0
+        process_mock.communicate.return_value = (
+            mocked_iocsh_subprocess_communicate_retval(module_name)
+        )
+        popen_mock.return_value = process_mock
+
         run_iocsh("iocsh", 2, tmp_file.as_posix())
-    assert "Loaded iocstats version" in caplog.text
-
-
-def test_run_iocsh_autosave(tmp_path: Path, caplog: pytest.LogCaptureFixture) -> None:
-    file_contents = """\
-require autosave
-
-epicsEnvSet("IOCNAME", "myioc")
-epicsEnvSet("AS_TOP", "/tmp")
-
-iocshLoad("$(autosave_DIR)/autosave.iocsh", "AS_TOP=$(AS_TOP), IOCNAME=$(IOCNAME)")
-"""
-    tmp_file = tmp_path / "test_autosave.cmd"
-    tmp_file.write_text(file_contents)
-
-    with caplog.at_level(logging.INFO):
-        run_iocsh("iocsh", 2, tmp_file.as_posix())
-    assert "Loaded autosave version" in caplog.text
+    assert f"Loaded {module_name} version" in caplog.text
--- tests/utils.py
+++
@@ -1,6 +0,0 @@
-import re
-
-
-def get_module_dir(logtext: str, module: str) -> str:
-    match = re.search(f"Module {module} version " + r".* found in (.*)\n", logtext)
-    return match.group(1) if match else ""
